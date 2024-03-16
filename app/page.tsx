8ef7167cb23418b8b10ef0b71374e355cd08ac3f
Mohamed Abdelmaksoud <medohmada1623@gmail.com>
2024-03-17 00:13:55 +0200

    Add social media icons and contact information to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,17 @@ import ServiceCard from "./components/ServiceCard";
 import { services } from "./mockups/services";
 import { serviceTypes } from "./mockups/serviceTypes";
 import ImageGallery from "react-image-gallery";
+import WhatsappIcon from "./icons/WhatsappIcon";
+import InstagramIcon from "./icons/InstagramIcon";
+import SnapchatIcon from "./icons/SnapchatIcon";
+import LocationPinIcon from "./icons/LocationPinIcon";
 
 export default function Home() {
   const [activeFilter, setActiveFilter] = useState("all");
   const [filteredServices, setFilteredServices] = useState(services);
-  const [paginatedServices, setPaginatedServices] = useState<any>(services.slice(0, 3));
+  const [paginatedServices, setPaginatedServices] = useState<any>(
+    services.slice(0, 3),
+  );
   const [page, setPage] = useState(1);
 
   useEffect(() => {
@@ -95,7 +101,7 @@ export default function Home() {
         </div>
 
         {/* Image carousel */}
-        <div className="p-4 bg-white rounded-2xl pb-9">
+        <div className="mb-10 rounded-2xl bg-white p-4 pb-9">
           <ImageGallery
             items={images}
             showThumbnails={false}
@@ -106,6 +112,48 @@ export default function Home() {
             autoPlay={true}
             slideDuration={500}
           />
+        </div>
+
+        {/* Stay In Contact */}
+        <div className="flex w-full flex-col gap-2 rounded-2xl bg-primary p-7">
+          <h2 className="text-center text-[28px] font-bold text-white">
+            ابق على تواصل معنا
+          </h2>
+          <h3 className="mb-1.5 text-center text-sm font-normal text-white/70">
+            تابعنا على وسائل التواصل الاجتماعية
+          </h3>
+          <a className="flex items-center gap-1.5 rounded-[20px] bg-white p-2.5" href="#">
+            <div className="flex h-7 w-7 items-center justify-center rounded-full bg-primary text-white">
+              <WhatsappIcon width={16} height={16} />
+            </div>
+            <span className="text-sm font-semibold text-darkGray">
+              تواصل معنا على الواتس اب
+            </span>
+          </a>
+          <a className="flex items-center gap-1.5 rounded-[20px] bg-white p-2.5" href="#">
+            <div className="flex h-7 w-7 items-center justify-center rounded-full bg-primary text-white">
+              <SnapchatIcon width={16} height={16} />
+            </div>
+            <span className="text-sm font-semibold text-darkGray">
+              تابعنا على سناب شات
+            </span>
+          </a>
+          <a className="flex items-center gap-1.5 rounded-[20px] bg-white p-2.5" href="#">
+            <div className="flex h-7 w-7 items-center justify-center rounded-full bg-primary text-white">
+              <InstagramIcon width={16} height={16} />
+            </div>
+            <span className="text-sm font-semibold text-darkGray">
+              تابعنا على انستقرام
+            </span>
+          </a>
+          <a className="flex items-center gap-1.5 rounded-[20px] bg-white p-2.5" href="#">
+            <div className="flex h-7 w-7 items-center justify-center rounded-full bg-primary text-white">
+              <LocationPinIcon width={16} height={16} />
+            </div>
+            <span className="text-sm font-semibold text-darkGray">
+              احصل على الموقع عبر خرائط جوجل
+            </span>
+          </a>
         </div>
       </div>
     </main>
